8995695c49f3fb8eb1b82e6f42f3dda5c7abcb3b
Matt Colen <mattcolen@gmail.com>
2019-03-30 11:57:30 -0700

    Calculate gradient in addition to cost

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -21,6 +21,7 @@ grad = zeros(size(theta));
 %
 h = sigmoid(X * theta);
 J = (1 / m) * sum(-y .* log(h) - (1 - y) .* log(1 - h));
+grad = (1 / m) * X' * (h - y);
 % =============================================================
 
 end
