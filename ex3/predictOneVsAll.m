a609f9d2dbf87a44cfab2481be47f8513f488783
Matt Colen <mattcolen@gmail.com>
2019-03-30 22:04:08 -0700

    Complete predictOneVsAll.m

--- ex3/predictOneVsAll.m
+++ ex3/predictOneVsAll.m
@@ -29,13 +29,8 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
-
-
-
-
-
+[h ih] = max(all_theta * X');
+p = ih';
 % =========================================================================
 
 
