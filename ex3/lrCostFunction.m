d75c01f472e7ea1f4cbc830e3260de6452217afd
Matt Colen <mattcolen@gmail.com>
2019-03-30 22:03:12 -0700

    Complete lrCostFunction.m

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -35,16 +35,13 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
-
-
-
-
-
-
-
-
-
-
+h = sigmoid(X * theta);
+% First compute regularized cost and gradient.
+J = (1 / m) * sum(-y .* log(h) - (1 - y) .* log(1 - h));
+grad = (1 / m) * X' * (h - y);
+% Now add regularization terms.
+J += lambda / (2 * m) * sum(theta(2:end) .^ 2);
+grad(2:end) += (lambda / m) * theta(2:end);
 % =============================================================
 
 grad = grad(:);
