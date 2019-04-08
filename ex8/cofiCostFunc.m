c3982a4b27f4fb4c1756bbf20b2856b55c5acfec
Matt Colen <mattcolen@gmail.com>
2019-04-07 23:24:01 -0700

    Compute gradients before regularization.

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -40,6 +40,22 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 J = sum(sum((X * Theta' - Y) .^ 2 .* R)) / 2;
+
+% Compute X_grad.
+for i = 1:size(X_grad, 1)
+  idx = find(R(i,:)==1);
+  Theta_temp = Theta(idx, :);
+  Y_temp = Y(i, idx);
+  X_grad(i,:) = (X(i,:) * Theta_temp' - Y_temp) * Theta_temp;
+end
+
+% Compute Theta_grad.
+for i = 1:size(Theta_grad,1)
+  idx = find(R(:,i)==1);
+  X_temp = X(idx, :);
+  Y_temp = Y(idx, i);
+  Theta_grad(i,:) = (X_temp * Theta(i,:)' - Y_temp)' * X_temp;
+end
 % =============================================================
 
 grad = [X_grad(:); Theta_grad(:)];
